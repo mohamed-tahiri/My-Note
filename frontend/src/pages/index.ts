fb182e687d5bd0b99dd71853c82990d1329fb6cd
mohamed.tahiri <mhdtahiri01@gmail.com>
2026-01-13 21:23:47 +0100

    feat(admin): add admin pages (overview, monitoring, analytics)

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as OverviewPage } from './admin/OverviewPage';
+export { default as MonitoringPage } from './admin/MonitoringPage';
+export { default as AnalyticsPage } from './admin/AnalyticsPage';
+export { default as SecurityPage } from './admin/SecurityPage';
+export { default as SettingsPage } from './admin/SettingsPage';
